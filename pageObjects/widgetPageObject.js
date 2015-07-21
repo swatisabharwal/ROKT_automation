a1d1522c8b8697538cf7f64b67de330a96e51919
Swati Sabharwal <swatisabharwal@HPDB0089.qait.com>
2015-07-21 17:55:04 +0530

    Added test cases

--- pageObjects/widgetPageObject.js
+++ pageObjects/widgetPageObject.js
@@ -1,6 +1,7 @@
-
+var WindowHandlers=require('../Utilities/WindowHandlers.js');	
 var WidgetPage = function () {
 	//Empty constructor for now, can add piece of code if required
+
 };
 
  WidgetPage.prototype = Object.create({}, {
@@ -10,6 +11,15 @@ var WidgetPage = function () {
     attendButton: { get: function () { return browser.driver.findElement(by.css('#ux_smartSignup_facebook_event_message_inner_buttons_attending')); }},
     widgetOverlay: { get: function () { return browser.driver.findElement(by.css('.ui_smartsignup.ui_module.tween_opacity')); }},
     shareLabel: { get: function () { return browser.driver.findElement(by.css('.ui_smartSignup_facebook_event_message_header_text')); }},
+    emailId:       { get: function () { return browser.driver.findElement(by.css('#email'));}},
+    password:      { get: function () { return browser.driver.findElement(by.css('#pass'));}},
+    loginBtn:      { get: function () { return browser.driver.findElement(by.css('#u_0_2')).click();}},
+	send_inviation_btn:{ get: function () { return browser.driver.findElement(by.xpath('//*[@id="ux_smartSignup_facebook_event_message_inner_buttons_send_invitation"]'));}},
+    check_box:     { get: function () { return browser.driver.findElement(by.xpath('//*[@id="ux_iagree_check"]'));}},
+    cont_btn:      { get: function () { return browser.driver.findElement(by.xpath('//button[@class="button submit large ui_smartsignup_buttons_next directive clickable"]'));}},
+    skip_btn:      { get: function () { return browser.driver.findElement(by.xpath('//*[@id="ux_surveyQuestionGroup_holder"]/form/div/div/div[1]/div[2]/button'));}},
+    cont_share_btn:{ get: function () { return browser.driver.findElement(by.xpath('//*[@id="ux_shareandwin_main_sns_skip_button"]'));}},   
+   
     
     
    // Methods around Object -- User actions 
@@ -32,9 +42,88 @@ var WidgetPage = function () {
     	value: function () { 
     		return this.attendButton.click();
     	}
-    }
+    },
     
+    fbWindowHandler: {
+	  value: function () {
+		var Whandlers =WindowHandlers();
+		return browser.driver.switchTo().window(Whandlers.popuphandler);   
+       }      
+   },
+    inputFbId: {
+        value: function () {
+		return this.emailId.sendKeys("testuser01.automation@gmail.com");
+        }
+    },
+  inputFbPass: {
+        value: function () {
+                return this.password.sendkeys("Qait@123");
+       }
+   },
+clickOnLoginBtn: {
+	  value: function () {
+		return this.loginBtn.click();
+       }
+   },     
+   
+verifySendInvitationButton: {
+	 value: function () {
+		var Whandlers =WindowHandlers();
+		browser.driver.switchTo().frame(this.widgetFrame);
+		return this.send_invitation_btn.isDisplayed();
+     
+      }
+   },
 
+clickCheckbBox:	{
+	 value: function () {
+		return this.check_box.click();
+     
+      }
+   },
+
+ verifyContBtn:	{
+	 value: function () {
+		return this.cont_btn.isDisplayed();
+     
+      }
+   },
+
+  clickContBtn:{
+	 value: function () {
+		return this.cont_btn.click();
+     
+      }
+   },
+
+   verifyOffer:	{
+	 value: function () {
+		return this.skip_btn.isDisplayed();
+     
+      }
+   },
+
+   clickOnSkip:	{
+	 value: function () {
+		return this.skip_btn.click();
+     
+      }
+   },
+
+    verifyCont:	{
+	 value: function () {
+		return this.cont_share_btn.isDisplayed();
+     
+      }
+   },
+
+ clickOnContBtn:	{
+	 value: function () {
+		return this.cont_share_btn.click();
+     
+      }
+   }
+    
 });
 
 module.exports = WidgetPage;
--- testCases/testToValidateAttendingFeature.js
+++ testCases/testToValidateAttendingFeature.js
@@ -3,33 +3,71 @@ var browserSession = require('../sessionInitiator/browserSession.js');
 
   describe('ROKT Widget Demo Test Using Protractor -- ', function () {
   var session = new browserSession();
-  var page = new WidgetPage();
+  var Wpage = new WidgetPage();
 	
 	it('Verify iFrame and switch to it', function () {	
-		expect(page.widgetFrame.isDisplayed()).toBe(true);
-		page.switchToWidgetFrame();
+		expect(Wpage.widgetFrame.isDisplayed()).toBe(true);
+		Wpage.switchToWidgetFrame();
 		console.log("User successfully switched to widget iFrame");
 
 	});
 
 
   	it('Verify widget Overlay appears', function () {
-  		expect(page.getVisibiltyOfWidgetOverlay()).toBe(true);
+  		expect(Wpage.getVisibiltyOfWidgetOverlay()).toBe(true);
   		console.log("Widget Overlay is correctly displayed");
   
 	});
 	
 	it('Verify the message -- Share on Facebook', function () {
-  		expect(page.getShareLabelText()).toBe('Share on Facebook');
+  		expect(Wpage.getShareLabelText()).toBe('Share on Facebook');
   		console.log("Application displays the message 'Share on Facebook' correctly on the widget");
   
 	});
 	
 	it('Click \'Attending\' Button and validate Facebook Auth screen ', function () {
-  		page.clickOnAttendButton();
+  		browser.sleep(5000);
+  		Wpage.clickOnAttendButton();
   		browser.sleep(5000);
   		console.log("Widget Overlay is correctly displayed");
   
+	});
+	
+	it('Handling Fb Window ', function () {
+		expect(Wpage.fbWindowHandler()).toBe('true');
+		browser.sleep(5000);
+		console.log("FaceBook Window is handled");
+
+	}); 
+
+	it('Entering Fb Credentials ', function () {
+		expect(Wpage.inputFbId()).toBe('true');
+		expect(Wpage.inputFbPass()).toBe('true');
+		expect(Wpage.clickOnLoginBtn()).toBe('true');
+		browser.sleep(5000);
+		console.log("LoggedIn into FaceBook");
+
+	});
+
+	it('Verifying SendInvitation Button ', function () {
+		expect(Wpage.verifySendInvitationButton()).toBe('true');
+		browser.sleep(5000);
+		console.log("Send Invitation Button Verified");	
+
+	});
+	
+	it('click on agreement checkbox' , function () {
+  		expect(Wpage.clickCheckbBox()).toBe('true');	
+		console.log('Ticked on CheckBox');
+
+	});
+
+	it('verify and click on continue' , function () {
+		expect(Wpage.verifyContBtn()).toBe('true');
+		console.log("Continue button is available");
+		expect(Wpage.clickContBtn()).toBe('true');
+		console.log("clicked on continue button");	
+
 	});
 
 });
